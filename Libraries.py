b8cef829c7036ad52431da1666694d233c329e67
Doddi Purushotham <140105932+Purushotham1912@users.noreply.github.com>
2025-01-30 08:38:10 +0530

    Create Libraries.py

---
+++ Libraries.py
@@ -0,0 +1,11 @@
+# Install Required Libraries
+# First, install the required libraries. Open a terminal or command prompt and run:
+
+# -----python code:-------
+pip install yfinance pandas sqlalchemy psycopg2
+
+
+# yfinance – Fetches stock data from Yahoo Finance
+# pandas – Data manipulation
+# sqlalchemy – Connects to PostgreSQL
+# psycopg2 – PostgreSQL adapter for Python
